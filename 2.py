09969dd1f2b5dec3badd33e7eb9be4e30d6c2d03
hashep <gooboo712@gmail.com>
2024-02-21 19:14:07 +0900

    jj

---
+++ 2.py
@@ -0,0 +1,9 @@
+a= 4.24E10 # 4.24 x 10^10
+
+print(a)
+b=24
+
+print(a*b)
+print(a**b)
+print(a%b)
+print(a/b)
---
+++ 3.py
@@ -0,0 +1,40 @@
+print("shit's")
+print("shit\"s") # \"
+print('shit"s3')
+
+megalul = "shit's2"
+print('aa\nbaa')
+
+megalul2 = '''what
+the
+buck
+'''
+
+megalul3 = """
+kkk
+bbb
+ffasaf"""
+print(megalul2)
+
+print(megalul3)
+
+print("="*50)
+print("Your Shit")
+print("="*50)
+print(megalul3[-1])
+print(megalul3[0:2])
+print(megalul3[0:7])
+
+omegalul = "what you"
+
+print(omegalul[0:1]) #w
+print(omegalul[0:4]) #what
+
+a = "%dkk" %3
+print(a)
+b = "%skk" %"ffff"
+print(b)
+c = "%s%d" %("sibal",2)
+print(c)
+d = "%d%% fuk" %333 #%% 3%
+print(d)
