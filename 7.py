5761aa5ecad16dfaea11380bd8364427684006f7
hashep <gooboo712@gmail.com>
2024-03-24 23:17:17 +0900

    faff

---
+++ 7.py
@@ -0,0 +1,8 @@
+money = True
+
+if money:
+    print("뭐인마")
+    print("뭐인마")
+    print("뭐인마")
+else:
+    print("아닌데요")
---
+++ 8.py
@@ -0,0 +1,11 @@
+a = [2, 3]
+card = True
+
+if 1 in a :
+    print("omg")
+elif card :
+    print("omg3")
+else :
+    print("omg2")
+    
+# elif는 if 혹은 또다른 elif 뒤에만 붙있수 있다. else 뒤에는 불가
