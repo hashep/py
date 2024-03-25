005e1c14a7ec805cfd81a6ca5842d1510b66bac1
hashep <gooboo712@gmail.com>
2024-03-25 15:22:21 +0900

    11

---
+++ 10.py
@@ -0,0 +1,11 @@
+prom = """ 
+1. shit
+2.   what
+3. asfafa
+
+Enter Number : """
+
+number = 0
+while number != 2 :
+    print(prom)
+    number = int(input())
---
+++ 9.py
@@ -0,0 +1,7 @@
+chop = 0
+while chop < 10 :
+    chop = chop + 1
+    print("나무찍기%d회" %chop)
+    if chop == 10 :
+        print("나무컽")
+        break
