330c0ad873a2e3ecce49f580ab65b238df6ab250
hashep <gooboo712@gmail.com>
2024-04-15 23:12:38 +0900

    Create 18.py

---
+++ 18.py
@@ -0,0 +1,20 @@
+def gay(a,b):
+    return a+b
+
+a=3
+b=4
+c=gay(a,b)
+d=gay(1,4)
+print(c+d)
+
+def say():
+    return 'Yo'
+
+a= say()
+print(a)
+
+def w2(a, b):
+    print("%d, %d의 합은 %d 입니다." %(a,b,a+b))
+
+w2(3,4)
+print(w2)
