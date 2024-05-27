b016a071f17fe9f14addddfcce1a785b2848b17c
hashep <gooboo712@gmail.com>
2024-05-27 15:45:07 +0900

    .

---
+++ num2.py
@@ -0,0 +1,12 @@
+A = int(input())
+
+if 101>A or A>=90:
+    print('A')
+elif 90>A or A>=80:
+    print('B')
+elif 80>A or A>=70:
+    print('C')
+elif 70>A or A>=60:
+    print('D')
+else:
+    print('F')
---
+++ num3.py
@@ -0,0 +1,6 @@
+A = int(input())
+
+if A%4==0 and (A%100!=0 or A%400==0): #wow
+    print(1)
+else:
+    print(0)
